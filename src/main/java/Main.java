27e0bce14aa875871e3fb8b0d6e67d0a9d664e80
Andrei <avg37t@gmail.com>
2021-02-28 18:25:56 +0300

    The second project on Kafka (With Docker) Version: 1.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,8 @@ import static by.epam.kafka.task2.constant.Constant.*;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println();
+
         Producer producer = new Producer();
         Consumer consumer1 = new Consumer("Consumer1", ".\\src\\main\\java\\first_consumer.txt");
         Consumer consumer2 = new Consumer("Consumer2", ".\\src\\main\\java\\second_consumer.txt");
